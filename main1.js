914dad20c015571494ae33ef49ecc9a3544466b8
realjacobryer <71294596+realjacobryer@users.noreply.github.com>
2021-02-16 20:50:41 -0500

    Add files via upload

---
+++ main1.js
@@ -0,0 +1,9 @@
+function familyAge(type, name, years) {
+	return `My ${type}, ${name}, is ${years} old. (${years * 12} months old.)`;
+}
+
+console.log(familyAge(father, Peter, 67));
+
+console.log(familyAge(mother, Martha, 56));
+
+console.log(familyAge(brtoher, Marc, 21));
